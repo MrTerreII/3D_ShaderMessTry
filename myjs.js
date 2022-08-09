c80610777220a1cec9279523648900955bb7b8c0
MrTerrell <ricky_21497@hotmail.com>
2022-08-09 18:55:09 +0200

    Advanced some code
    Have to look at the line 250 and probably 251. Mesh doesn't have position because idk

--- myjs.js
+++ myjs.js
@@ -5,6 +5,65 @@ import { DragControls } from 'https://cdn.jsdelivr.net/npm/three@0.143/examples/
 
 //@ts-check
 
+class RigidBody {
+    constructor() {
+    }
+
+    setRestitution(val) {
+        this.body_.setRestitution(val);
+    }
+
+    setFriction(val) {
+        this.body_.setFriction(val);
+    }
+
+    setRollingFriction(val) {
+        this.body_.setRollingFriction(val);
+    }
+
+    createBox(mass, pos, quat, size) {
+        this.transform_ = new Ammo.btTransform();
+        this.transform_.setIdentity();
+        this.transform_.setOrigin(new Ammo.btVector3(pos.x, pos.y, pos.z));
+        this.transform_.setRotation(new Ammo.btQuaternion(quat.x, quat.y, quat.z, quat.w));
+        this.motionState_ = new Ammo.btDefaultMotionState(this.transform_);
+
+        const btSize = new Ammo.btVector3(size.x * 0.5, size.y * 0.5, size.z * 0.5);
+        this.shape_ = new Ammo.btBoxShape(btSize);
+        this.shape_.setMargin(0.05);
+
+        this.inertia_ = new Ammo.btVector3(0, 0, 0);
+        if (mass > 0) {
+            this.shape_.calculateLocalInertia(mass, this.inertia_);
+        }
+
+        this.info_ = new Ammo.btRigidBodyConstructionInfo(
+            mass, this.motionState_, this.shape_, this.inertia_);
+        this.body_ = new Ammo.btRigidBody(this.info_);
+
+        Ammo.destroy(btSize);
+    }
+
+    createSphere(mass, pos, size) {
+        this.transform_ = new Ammo.btTransform();
+        this.transform_.setIdentity();
+        this.transform_.setOrigin(new Ammo.btVector3(pos.x, pos.y, pos.z));
+        this.transform_.setRotation(new Ammo.btQuaternion(0, 0, 0, 1));
+        this.motionState_ = new Ammo.btDefaultMotionState(this.transform_);
+
+        this.shape_ = new Ammo.btSphereShape(size);
+        this.shape_.setMargin(0.05);
+
+        this.inertia_ = new Ammo.btVector3(0, 0, 0);
+        if (mass > 0) {
+            this.shape_.calculateLocalInertia(mass, this.inertia_);
+        }
+
+        this.info_ = new Ammo.btRigidBodyConstructionInfo(mass, this.motionState_, this.shape_, this.inertia_);
+        this.body_ = new Ammo.btRigidBody(this.info_);
+    }
+}
+
 class WorldMeshShaders {
     /* Physics constants of the World */
     gravityConstant = - 9.8;
@@ -85,26 +144,42 @@ class WorldMeshShaders {
 
         this._scene.background = textureEquirec;
 
-        this.plane = new THREE.Mesh(
-            new THREE.PlaneGeometry(200, 200, 10, 10),
-            new THREE.MeshStandardMaterial({
-                color: 0xFFFFFF,
-            }));
-        this.plane.castShadow = false;
-        this.plane.receiveShadow = true;
-        this.plane.rotation.x = -Math.PI / 2;
-        this._scene.add(this.plane);
+        const ground = new THREE.Mesh(
+            new THREE.BoxGeometry(100, 1, 100),
+            new THREE.MeshStandardMaterial({ color: 0x404040 }));
+        ground.castShadow = false;
+        ground.receiveShadow = true;
+        this._scene.add(ground);
 
-        this.box1 = new THREE.Mesh(
+        const rbGround = new RigidBody();
+        rbGround.createBox(0, ground.position, ground.quaternion, new THREE.Vector3(100, 1, 100));
+        rbGround.setRestitution(0.99);
+        this.physicsWorld.addRigidBody(rbGround.body_);
+
+        this.rigidBodies_ = [];
+
+        const box = new THREE.Mesh(
             new THREE.BoxGeometry(2, 2, 2),
             new THREE.MeshStandardMaterial({
                 color: 0xFF0000,
             }));
-        this.box1.position.set(0, 1, 0);
-        this.box1.castShadow = true;
-        this.box1.receiveShadow = true;
-        this._scene.add(this.box1);
-        this.boxes.push(this.box1);
+        box.position.set(0, 10, 0);
+        box.castShadow = true;
+        box.receiveShadow = true;
+        
+        const rbBox = new RigidBody();
+        rbBox.createBox(5, box.position, box.quaternion, new THREE.Vector3(2, 2, 2), null);
+        rbBox.setRestitution(0.25);
+        rbBox.setFriction(1);
+        rbBox.setRollingFriction(5);
+        this.physicsWorld.addRigidBody(rbBox.body_);
+        
+        this._scene.add(this.box);
+
+        this.boxes.push(this.box);
+
+        this.rigidBodies_.push({mash: box, rigidBody: rbBox});
+
         this.dragcontrol = new DragControls([... this.boxes], this._camera, this._threejs.domElement);
         this.dragcontrol.addEventListener('dragstart', () => {
             this.startfalling = false;
@@ -116,6 +191,9 @@ class WorldMeshShaders {
             this.controls.enabled = true;
             this.clock.start();
         });
+
+        this.tmpTransform_ = new Ammo.btTransform();
+
         this.countdown = 1.0;
         this.count = 0;
         this.previousRAF_ = null;
@@ -123,7 +201,7 @@ class WorldMeshShaders {
     }
 
     _InitPhysics() {
-        const collisionConfiguration = new Ammo.btDefaultCollisionConfiguration();
+        const collisionConfiguration = new Ammo.btSoftBodyRigidBodyCollisionConfiguration();
         const dispatcher = new Ammo.btCollisionDispatcher(collisionConfiguration);
         const broadphase = new Ammo.btDbvtBroadphase();
         const solver = new Ammo.btSequentialImpulseConstraintSolver();
@@ -131,7 +209,6 @@ class WorldMeshShaders {
         this.physicsWorld = new Ammo.btSoftRigidDynamicsWorld(dispatcher, broadphase, solver, collisionConfiguration, softBodySolver);
         this.physicsWorld.setGravity(new Ammo.btVector3(0, this.gravityConstant, 0));
         this.physicsWorld.getWorldInfo().set_m_gravity(new Ammo.btVector3(0, this.gravityConstant, 0));
-        this.transformAux1 = new Ammo.btTransform();
         this.softBodyHelpers = new Ammo.btSoftBodyHelpers();
     }
 
@@ -143,7 +220,7 @@ class WorldMeshShaders {
 
     _RAF() {
         requestAnimationFrame((time) => {
-            if (this.previousRAF_ === null){
+            if (this.previousRAF_ === null) {
                 this.previousRAF_ = time;
             }
             this._STEP(time - this.previousRAF_);
@@ -155,15 +232,26 @@ class WorldMeshShaders {
 
     _STEP(timeElapsed) {
         const timeElapsedS = timeElapsed * 0.001;
-        this.physicsWorld.stepSimulation(timeElapsedS, 10);
-
+        
         this.countdown -= timeElapsedS;
-        if (this.countdown < 0 && this.count < 10){
+        if (this.countdown < 0 && this.count < 10) {
             this.countdown = 0.25;
             this.count += 1;
         }
+
+        this.physicsWorld.stepSimulation(timeElapsedS, 10);
+        for (let i = 0; i < this.rigidBodies_.length; ++i) {
+            this.rigidBodies_[i].rigidBody.motionState_.getWorldTransform(this.tmpTransform_);
+            const pos = this.tmpTransform_.getOrigin();
+            const quat = this.tmpTransform_.getRotation();
+            const pos3 = new THREE.Vector3(pos.x(), pos.y(), pos.z());
+            const quat3 = new THREE.Quaternion(quat.x(), quat.y(), quat.z(), quat.w());
+      
+            this.rigidBodies_[i].mesh.position.copy(pos3);
+            this.rigidBodies_[i].mesh.quaternion.copy(quat3);
+          }
     }
-    
+
 }
 
 let _APP = null;
